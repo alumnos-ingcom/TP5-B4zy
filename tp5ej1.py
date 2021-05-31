cefdf2e9c2443874a637830f52d4f30f2dfa139f
Tomás Gadea <39812448+B4zy@users.noreply.github.com>
2021-05-31 20:08:21 -0300

    Add files via upload

---
+++ tp5ej1.py
@@ -0,0 +1,31 @@
+################
+# Tomás Gadea - @B4zy
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def es_par(numero):
+    comprobacion = numero / 2
+    if comprobacion - int(comprobacion) == 0:
+        return True
+    else:
+        return False
+        
+        
+def prueba():
+    while True:
+        numero_ingresado = input("Ingrese un numero: ")
+        try:
+            numero_ingresado = int(numero_ingresado)
+            break
+        except ValueError:
+            print("Oops, eso no era un numero")
+            
+    comprobar = es_par(numero_ingresado)
+    
+    if comprobar == True:
+        print(f"[{numero_ingresado}] Es par.")
+    else:
+        print(f"[{numero_ingresado}] No es par.")
+
+prueba()
